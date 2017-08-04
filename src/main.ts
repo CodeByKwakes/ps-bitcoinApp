4771120db65d7d09ed4e7865d14c260679253d24
Kwakes Prempeh <kwaku.prempeh@holition.com>
2017-08-04 13:51:08 +0100

    app working. now to set up it up corrcetly

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import * as express from 'express'
+import * as express from 'express';
 
-let app = express();
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World! New World');
 });
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
